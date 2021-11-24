751a01310b02cf3816c4a600a560d76ef8112893
JeanCGPontes <jeanestudos.je@gmail.com>
2021-11-24 15:50:05 -0300

    Functions

--- functions.py
+++ functions.py
@@ -7,3 +7,11 @@ def get_font(font_name: str, bold: bool, font_size: int):
     font.setBold(bold)
     font.setPointSize(font_size)
     return font
+
+
+def solve_percentage(math_expression):
+    operator_location = math_expression.find('%')
+    number_1 = int(math_expression[0:operator_location])
+    number_2 = int(math_expression[operator_location+1:len(math_expression)])
+    result = (number_1 * number_2)/100
+    return result
