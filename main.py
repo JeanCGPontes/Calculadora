c642f025b8955d7dd8e1d93cefcb4aba5808a5e4
JeanCGPontes <jeanestudos.je@gmail.com>
2021-11-30 13:27:27 -0300

    Main

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import classes
-import sys
 
 
 if __name__ == '__main__':
-    application = classes.QtWidgets.QApplication(sys.argv)
-    window = classes.MainWindow()
-    sys.exit(application.exec_())
+    classes.Calculator()
