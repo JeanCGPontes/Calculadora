6e30a95555eb58d77a521952bfc72aa2a91991c3
JeanCGPontes <jeanestudos.je@gmail.com>
2021-11-30 13:27:17 -0300

    Functions

--- functions.py
+++ functions.py
@@ -19,25 +19,37 @@ def solve_percentage(math_expression):
 
 def return_theme(theme):
     if theme == 'dark':
-        window_theme = 'background-color: rgb(21, 21, 21);'
+        window_theme = ("""QMainWindow {background-color: rgb(21, 21, 21);}  
+                           QMenuBar {background-color: rgb(21, 21, 21);
+                                     color: rgb(255, 255, 255)}
+                           QMenuBar::item{background-color: transparent}
+                           QMenuBar::item:selected{background-color: rgb(206, 206, 206);
+                                                   color: rgb(0, 0, 0)}
+                           QMenu {background-color: rgb(255, 255, 255);
+                                  color: rgb(0, 0, 0)}
+                           QMenu::item:selected{background-color: rgb(225, 225, 225)}""")
+
         number_button_style = ("""QPushButton {background-color: rgb(206,206,206);
                                                        border-color: rgb(166, 166, 166);
                                                        border-width: 1px;
                                                        border-style:solid;
                                                        border-radius: 4;}
                                           QPushButton:hover {background-color: rgb(228, 228, 228)}""")
+
         style_of_math_operation_buttons = ("""QPushButton {background-color: rgb(96, 166, 111);
                                                                    border-color: rgb(166, 166, 166);
                                                                    border-width: 1px;
                                                                    border-style:solid;
                                                                    border-radius: 3;}
                                                       QPushButton:hover {background-color: rgb(96, 166, 111, 200)}""")
+
         equal_button_style = ("""QPushButton {background-color: rgb(107, 197, 107);
                                                       border-color: rgb(166, 166, 166);
                                                       border-width: 1px;
                                                       border-style: solid;
                                                       border-radius: 4;}
                                          QPushButton:hover {background-color: rgba(107, 197, 107, 200);}""")
+
         label_style = ("""color: rgb(206, 206, 206);
                                   background-color: rgb(23, 23, 23);    
                                   border-color: rgb(166, 166, 166);
@@ -48,40 +60,53 @@ def return_theme(theme):
         return window_theme, number_button_style, style_of_math_operation_buttons, equal_button_style, label_style
 
     elif theme == 'light':
-        window_theme = 'background-color: rgb(241, 244, 243);'
+        window_theme = ("""QMainWindow {background-color: rgb(241, 244, 243)}  
+                           QMenuBar {background-color: rgb(241, 244, 243)}
+                           QMenuBar::item{background-color: transparent}
+                           QMenuBar::item:selected{background-color: rgb(255, 255, 255)}
+                           QMenu {background-color: rgb(255, 255, 255);
+                                  color: rgb(0, 0, 0)}
+                           QMenu::item:selected{background-color: rgb(225, 225, 225)}""")
+
         number_button_style = ("""QPushButton {background-color: rgb(255, 255, 255);
-                                                       border-color: rgb(166, 166, 166);
-                                                       border-width: 1px;
-                                                       border-style:solid;
-                                                       border-radius: 3;}
+                                               border-color: rgb(166, 166, 166);
+                                               border-width: 1px;
+                                               border-style:solid;
+                                               border-radius: 3;}
+                            QPushButton:hover {background-color: rgb(235, 235, 235)}
+                         QPushButton:pressed  {background-color: rgb(235, 235, 235)}""")
 
-                                          QPushButton:hover {background-color: rgb(235, 235, 235)}
-
-                                          QPushButton:pressed {{background-color: rgb(235, 235, 235)}}""")
         style_of_math_operation_buttons = ("""QPushButton {background-color: rgba(13, 202, 174, 168);
-                                                                   border-color: rgb(166, 166, 166);
-                                                                   border-width: 1px;
-                                                                   border-style:solid;
-                                                                   border-radius: 3;}
+                                                           border-color: rgb(166, 166, 166);
+                                                           border-width: 1px;
+                                                           border-style:solid;
+                                                           border-radius: 3;}
+                                        QPushButton:hover {background-color: rgb(13, 202, 174, 200)}""")
 
-                                                      QPushButton:hover {background-color: rgb(13, 202, 174, 200)}""")
         equal_button_style = ("""QPushButton {background-color: rgba(0, 135, 159, 180);
-                                         border-color: rgb(166, 166, 166);
-                                         border-width: 1px;
-                                         border-style: solid;
-                                         border-radius: 3;}
+                                              border-color: rgb(166, 166, 166);
+                                              border-width: 1px;
+                                              border-style: solid;
+                                              border-radius: 3;}
+                           QPushButton:hover {background-color: rgba(0, 135, 159, 200);}""")
 
-                                         QPushButton:hover {background-color: rgba(0, 135, 159, 200);}""")
         label_style = ("""background-color: rgb(252, 252, 252);
-                                  border-color: rgb(166, 166, 166);
-                                  border-width: 1px;
-                                  border-style:solid;
-                                  border-radius: 3;""")
+                          border-color: rgb(166, 166, 166);
+                          border-width: 1px;
+                          border-style:solid;
+                          border-radius: 3;""")
 
         return window_theme, number_button_style, style_of_math_operation_buttons, equal_button_style, label_style
 
     else:
-        window_theme = 'background-color: rgb(241, 244, 243);'
+        window_theme = ("""QMainWindow {background-color: rgb(241, 244, 243)}  
+                                  QMenuBar {background-color: rgb(241, 244, 243)}
+                                  QMenuBar::item{background-color: transparent}
+                                  QMenuBar::item:selected{background-color: rgb(255, 255, 255)}
+                                  QMenu {background-color: rgb(255, 255, 255);
+                                         color: rgb(0, 0, 0)}
+                                  QMenu::item:selected{background-color: rgb(225, 225, 225)}""")
+
         number_button_style = ("""QPushButton {background-color: rgb(255, 255, 255);
                                                                border-color: rgb(166, 166, 166);
                                                                border-width: 1px;
@@ -91,6 +116,7 @@ def return_theme(theme):
                                                   QPushButton:hover {background-color: rgb(235, 235, 235)}
 
                                                   QPushButton:pressed {{background-color: rgb(235, 235, 235)}}""")
+
         style_of_math_operation_buttons = ("""QPushButton {background-color: rgba(13, 202, 174, 168);
                                                                            border-color: rgb(166, 166, 166);
                                                                            border-width: 1px;
@@ -98,6 +124,7 @@ def return_theme(theme):
                                                                            border-radius: 3;}
 
                                                               QPushButton:hover {background-color: rgb(13, 202, 174, 200)}""")
+
         equal_button_style = ("""QPushButton {background-color: rgba(0, 135, 159, 180);
                                                  border-color: rgb(166, 166, 166);
                                                  border-width: 1px;
@@ -105,6 +132,7 @@ def return_theme(theme):
                                                  border-radius: 3;}
 
                                                  QPushButton:hover {background-color: rgba(0, 135, 159, 200);}""")
+
         label_style = ("""background-color: rgb(252, 252, 252);
                                           border-color: rgb(166, 166, 166);
                                           border-width: 1px;
